6111caf4d2e2b4e1af238de02ba6ce1b1bcd9d90
Musale Martin <martinmusale@microsoft.com>
2025-03-27 01:05:56 +0300

    Add a function to book the event

--- BookEvents/EventsPlugin.cs
+++ BookEvents/EventsPlugin.cs
@@ -5,11 +5,21 @@ using Microsoft.SemanticKernel;
 
 public class EventsPlugin
 {
-    [KernelFunction("get_events")]
+    [KernelFunction("get_nairobi_events")]
     [Description("Gets a list of events in Nairobi")]
-    public async Task<List<Event>> GetLightsAsync()
+    public async Task<List<Event>> GetNairobiEventsAsync()
     {
         NairobiEvents events = new();
         return await events.GetEventsAsync();
+    }
+
+    [KernelFunction("book_nairobi_event")]
+    [Description("Books a nairobi event given the event id")]
+    [return: Description("returns the booked event")]
+    public async Task<Event> BookNairobiEvent([Description("the ID of the event that is going to be booked")] int eventId)
+    {
+        NairobiEvents nrb = new();
+        var events = await nrb.GetEventsAsync();
+        return events.FirstOrDefault(e => e.ID == eventId);
     }
 }
